666b7390fa519b90ac323be242a43866dccdf1e3
vnikolaou <vnikolaou12@gmail.com>
2018-04-28 23:44:05 +0300

    ...

---
+++ src/main/java/nullapps/pricematchers/sample/rest1/Rest1Application.java
@@ -0,0 +1,19 @@
+package nullapps.pricematchers.sample.rest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Rest1Application {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Rest1Application.class, args);
+	}
+}
